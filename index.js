e328ab130f52c6880b266fe30209c8c7da4223f4
Jesschuck <jaayschuck@gmail.com>
2023-11-08 21:24:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+class heroi{
+    constructor(nomeHeroi, idadeHeroi, tipoHeroi){
+        this.nomeHeroi = nomeHeroi;
+        this.idadeHeroi = idadeHeroi;
+        this.tipoHeroi = tipoHeroi;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipoHeroi) {
+          case "mago":
+            ataque = "magia";
+            break;
+          case "guerreiro":
+            ataque = "espada";
+            break;
+          case "monge":
+            ataque = "artes marciais";
+            break;
+          case "ninja":
+            ataque = "shuriken";
+            break;
+          default:
+            ataque = "usou um ataque genérico";
+        }
+        return(`O ${this.tipoHeroi} atacou usando ${ataque}`);
+    }
+}
+
+let mago = new heroi("Gandalf", 3.000, "mago")
+let guerreiro = new heroi("Aragorn", 45, "guerreiro")
+let monge = new heroi("Buda", 80, "monge")
+let ninja = new heroi("Naruto", 18, "ninja")
+
+console.log(mago.atacar())
+console.log(guerreiro.atacar())
+console.log(monge.atacar())
+console.log(ninja.atacar())
